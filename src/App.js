7ca62ce3796cfa14a340591b71b94ad9227bbc0d
Admin <minhajumid987@gmail.com>
2022-05-10 20:51:15 +0600

    intalled materilize and added card

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
 import React, { useEffect } from "react";
-import { useSelector,useDispatch } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Home from "./components/home";
-import {getAllData} from "./store/card-slice"
-
+import { getAllData } from "./store/card-slice";
+import M from "materialize-css";
 function App() {
     const dispatch = useDispatch();
     const alldata = useSelector((state) => state.card);
-    
+
     useEffect(() => {
         dispatch(getAllData());
     }, [dispatch]);
 
-    
     console.log(alldata.launchList);
     return (
         <>
-        <div className="container">
-      <div className="row g-3">
-        {alldata.launchList &&
-          alldata.launchList.length > 0 &&
-          alldata.launchList.map((lunch) => {
-            return (
-              <div className="col-12 col-md-6 col-lg-4">
-                 <div className="card h-100">
-      <img src={lunch.links.mission_patch_small} className="card-img-top" alt="lunch-img" />
-      <div className="card-body">
-        <h5 className="card-title">{lunch.mission_name}</h5>
-        <p className="card-text">{lunch.details}</p>
-      </div>
+         <nav>
+    <div class="nav-wrapper">
+      <form>
+        <div class="input-field">
+          <input id="search" type="search" required></input>
+          <label class="label-icon" for="search"><i class="material-icons">search</i></label>
+          <i class="material-icons">close</i>
+        </div>
+      </form>
     </div>
-              </div>
-            );
-          })}
-      </div>
-    </div>
+  </nav>
+
+            <div class="row">
+                {alldata.launchList &&
+                    alldata.launchList.length > 0 &&
+                    alldata.launchList.map((lunch) => {
+                        return (
+                            <div class="col s6 m3">
+                                <div class="card">
+                                    <div class="card-image waves-effect waves-block waves-light" >
+                                        <img
+                                            class="activator"
+                                            src={
+                                                lunch.links.mission_patch_small
+                                            }
+                                        ></img>
+                                    </div>
+                                    <div class="card-content">
+                                        <span class="card-title activator grey-text text-darken-4" style={{textOverflow: 'ellipsis',whiteSpace: 'nowrap',overflow: 'hidden'}}>
+                                            {lunch.mission_name}
+                                            <i class="material-icons right">
+                                                more_vert
+                                            </i>
+                                        </span>
+                                        <p>
+                                            <a href={lunch.links.wikipedia}>Wikipedia Link</a>
+                                        </p>
+                                    </div>
+                                    <div class="card-reveal">
+                                        <span class="card-title grey-text text-darken-4">
+                                            {lunch.flight_number}
+                                            <i class="material-icons right">
+                                                close
+                                            </i>
+                                        </span>
+                                        <p >{lunch.details}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+            </div>
             <Home></Home>
         </>
     );
